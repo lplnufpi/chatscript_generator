3ddc3b19571c5d68a8b7ac522da4e4901997cbc7
Jpegx100 <jpegx100@gmail.com>
2019-11-27 16:32:52 -0300

    saving and searching for saved input/selected rules pairs

--- templates/save_input.py
+++ templates/save_input.py
@@ -6,12 +6,13 @@ db = dataset.connect('sqlite:///user_inputs.db')
 table = db['inputs']
 entrada = sys.argv[1].replace('**', ' ')
 regra = sys.argv[3][1:] if len(sys.argv) == 4 else ''
+topic = sys.argv[2].split('_gen')[0]
 
 if(len(sys.argv) < 2):
     exit()
 elif((len(sys.argv) == 2)):
     table.insert(dict(entrada_usuario=entrada, topico="", regra=regra))
 elif((len(sys.argv) == 3)):
-    table.insert(dict(entrada_usuario=entrada, topico=sys.argv[2], regra=regra))
+    table.insert(dict(entrada_usuario=entrada, topico=topic, regra=regra))
 elif((len(sys.argv) == 4)):
-    table.insert(dict(entrada_usuario=entrada, topico=sys.argv[2], regra=regra))
+    table.insert(dict(entrada_usuario=entrada, topico=topic, regra=regra))
